023872fd6b934eec6601144dba70c3531131b4c4
DESKTOP-7E8ENMV\moish <moises.dev08@gmail.com>
2021-05-05 17:58:29 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {StyleSheet} from 'react-native';
+import {createStore} from 'redux';
+import Counter from './src/components/Counter';
+import {Provider} from 'react-redux';
+
+const initialState = {
+  counter: 0,
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREASE_COUNTER':
+      return {counter: state.counter + 1};
+    case 'DECREASE_COUNTER':
+      return {counter: state.counter - 1};
+  }
+  return state;
+};
+
+const store = createStore(reducer);
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
+  }
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
